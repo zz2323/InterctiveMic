95b35203539317403bdb740536848fe0e4034436
ZhihaoZhang <zz2323@columbia.edu>
2015-04-11 19:57:31 -0400

    added data to MicData.

--- MicData.js
+++ MicData.js
@@ -1,1 +1,7 @@
-data
+var MicData = {
+	    "Section":"Policy",
+        "MainHeadline":"Disappointing Jobs Report Shows the Economic Recovery Is Still Fragile",
+        "Excerpt":"Just 126,000 jobs added in March.",
+        "Author":"Zeeshan Aleem",
+        "UpdatedTime":"April 3, 2015"
+}
