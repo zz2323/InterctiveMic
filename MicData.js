bf62c2db4c58b449d16e927306574ab0ad492d09
ZhihaoZhang <zz2323@columbia.edu>
2015-04-11 20:03:48 -0400

    added addtional data and comments

--- MicData.js
+++ MicData.js
@@ -1,7 +1,24 @@
-var MicData = {
-	    "Section":"Policy",
-        "MainHeadline":"Disappointing Jobs Report Shows the Economic Recovery Is Still Fragile",
-        "Excerpt":"Just 126,000 jobs added in March.",
-        "Author":"Zeeshan Aleem",
-        "UpdatedTime":"April 3, 2015"
+var MicData = [
+{
+"Section":"Policy",
+"MainHeadline":"Disappointing Jobs Report Shows the Economic Recovery Is Still Fragile",
+"Excerpt":"Just 126,000 jobs added in March.",
+"Author":"Zeeshan Aleem",
+"UpdatedTime":"April 3, 2015"
+},
+{
+"Section":"Policy",
+"MainHeadline":"Hillary Clinton Will Announce She's Running for President on Sunday",
+"Excerpt":"It begins.",
+"Author":"Gregory Krieg",
+"UpdatedTime":"April 10, 2015"
+
+},
+{
+"Section":"Policy",
+"MainHeadline":"7 Harmful Myths We Need to Stop Telling About Poverty",
+"Excerpt":"It's time to look at the facts about the nature of poverty.",
+"Author":"Zeeshan Aleem",
+"UpdatedTime":"April 9, 2015"
 }
+]//end of MicData
